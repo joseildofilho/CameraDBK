04555eede4cc409b4a6577bb97800724141d40fa
joseildo filho <joseildofilho@yahoo.com.br>
2018-12-04 11:52:50 -0300

    adicionando opção de alterar nome dos arquivos e o local onde ele será salvo

--- app/src/main/java/armazemparaiba/com/br/cameradbk/MainActivity.kt
+++ app/src/main/java/armazemparaiba/com/br/cameradbk/MainActivity.kt
@@ -7,21 +7,23 @@ import android.os.Environment
 import android.se.omapi.Session
 import android.support.v4.app.ActivityCompat
 import android.util.Log
+import armazemparaiba.com.br.cameradbk.R.id.camera
 import com.otaliastudios.cameraview.SessionType
 import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.alert
-import org.jetbrains.anko.toast
+import org.jetbrains.anko.*
 import java.io.File
 import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
 
     private var recording = false
-    private val file = File(Environment.getExternalStorageDirectory().absolutePath + "/video.mp4")
+    private val file = createFolder()
+
+    private var quant:Int = 0;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         ActivityCompat.requestPermissions(this,
                 arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE,
                         Manifest.permission.READ_EXTERNAL_STORAGE,
@@ -34,16 +36,33 @@ class MainActivity : AppCompatActivity() {
         camera.addCameraListener(VideoListener())
         btn_toggle_record.setOnClickListener {
             if (!recording) {
-                camera.startCapturingVideo(file)
+                var nome = txt_nome.text.toString()
+                camera.startCapturingVideo(getPlace(nome))
+                txt_nome.isFocusable = false
+                txt_nome.isEnabled = false
                 toast("Gravando")
             } else {
                 camera.stopCapturingVideo()
+                txt_nome.isFocusable = true
+                txt_nome.isEnabled = true
                 toast("Gravação parada")
             }
             recording = recording.not()
 
         }
 
+    }
+
+    private fun createFolder():File {
+        val f = File(Environment.getExternalStorageDirectory(),"/videos")
+        if (!f.exists())
+            f.mkdir()
+        return f
+    }
+
+    private fun getPlace(nome:String): File {
+        quant += 1
+        return File(file, "$nome#$quant.mp4")
     }
 
     override fun onResume() {
