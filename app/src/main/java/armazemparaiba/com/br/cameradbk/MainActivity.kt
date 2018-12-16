e38d2d65f40d5c283e945a5511f3d43b934c36aa
joseildo filho <joseildofilho@yahoo.com.br>
2018-12-16 00:28:00 -0300

    adicionando Timer

--- app/src/main/java/armazemparaiba/com/br/cameradbk/MainActivity.kt
+++ app/src/main/java/armazemparaiba/com/br/cameradbk/MainActivity.kt
@@ -1,6 +1,7 @@
 package armazemparaiba.com.br.cameradbk
 
 import android.Manifest
+import android.app.Service
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
@@ -11,18 +12,23 @@ import armazemparaiba.com.br.cameradbk.R.id.camera
 import armazemparaiba.com.br.cameradbk.dataset.DatasetManager
 import com.otaliastudios.cameraview.SessionType
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.coroutines.delay
 import org.jetbrains.anko.*
+import org.jetbrains.anko.coroutines.experimental.bg
 import java.io.File
 import java.io.FileOutputStream
+import java.lang.Thread.sleep
+import java.util.*
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.locks.Lock
 
 class MainActivity : AppCompatActivity() {
 
-    private var recording = false
     private val file = createFolder()
-
-    private var quant:Int = 0;
-
+    private var quant:Int = 0
     private val dataset: DatasetManager = DatasetManager(Environment.getExternalStorageDirectory())
+    private var time: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,16 +39,28 @@ class MainActivity : AppCompatActivity() {
                     Manifest.permission.RECORD_AUDIO),
                 0)
 
+        setupCamera()
 
+        resetTimer()
+
+        btn_settings.setOnClickListener {
+            toast("não implementado ainda!")
+        }
+
+    }
+
+    private fun setupCamera() {
         camera.setLifecycleOwner(this)
         camera.sessionType = SessionType.VIDEO
+        camera.videoMaxDuration = 6000
         camera.addCameraListener(VideoListener())
         btn_toggle_record.setOnClickListener {
-            if (!recording) {
+            if (camera.isCapturingVideo.not()   ) {
                 var nome = txt_nome.text.toString()
                 val split = nome.split(",", "/").map {
                     it.replace(" ", "")
                 }
+                resetTimer()
                 camera.startCapturingVideo(dataset.getFile(split[0],split[1], split[2]))
                 txt_nome.isFocusable = false
                 txt_nome.isEnabled = false
@@ -53,10 +71,25 @@ class MainActivity : AppCompatActivity() {
                 txt_nome.isEnabled = true
                 toast("Gravação parada")
             }
-            recording = recording.not()
-
         }
 
+        doAsync {
+            while (true) {
+                if(camera.isCapturingVideo) {
+                    val delta = (System.currentTimeMillis() - time)
+                    uiThread {
+                        txt_time.text = "${delta / 1000 / 60}:${delta / 1000 % 60}"
+                    }
+                } else {
+                    Thread.sleep(200L)
+                }
+            }
+        }
+    }
+
+    private fun resetTimer() {
+        time = System.currentTimeMillis()
+        txt_time.text = "00:00"
     }
 
     private fun createFolder():File {
